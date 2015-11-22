647968a7bc00abf6b0d277757210462d4f0ea4c5
juno <juno@krasava.fi>
2015-11-22 08:56:54 +0200

    dev

--- src/app/app.js
+++ src/app/app.js
@@ -10,19 +10,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
-//import {Zippy} from 'app/zippy';
+var http_1 = require('angular2/http');
 var Hero = (function () {
     function Hero() {
     }
     return Hero;
 })();
 var AppComponent = (function () {
-    function AppComponent() {
+    //    public heroes = HEROES;
+    function AppComponent(http) {
+        var _this = this;
         this.title = 'Volkov Alexander';
-        this.heroes = HEROES;
+        http.get('activity.json')
+            .map(function (res) { return res.json(); }).subscribe(function (heroes) { return _this.heroes = heroes; });
+        // Subscribe to the observable to get the parsed people object and attach it to the
+        // component
+        //      .subscribe(people => this.people = people);
     }
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
+        //         console.log(JSON.stringify(HEROES) );
     };
     AppComponent.prototype.getSelectedClass = function (hero) {
         return { 'selected': hero === this.selectedHero };
@@ -31,24 +38,24 @@ var AppComponent = (function () {
         angular2_1.Component({
             selector: 'my-app',
             templateUrl: 'templates/main.html',
-            //    styleUrls:['style.css'],
+            viewProviders: [http_1.HTTP_PROVIDERS],
             directives: [angular2_1.CORE_DIRECTIVES, angular2_1.FORM_DIRECTIVES]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], AppComponent);
     return AppComponent;
 })();
 angular2_1.bootstrap(AppComponent);
-var HEROES = [
-    { "id": 1, "name": "Translation", "details": "details ksksksks" },
-    { "id": 2, "name": "Consultation", "details": "details jsjsjsjs" },
-    { "id": 3, "name": "Company Creation", "details": "details 18182828" },
-    { "id": 4, "name": "Activity 4", "details": "details hshs828282" },
-    { "id": 5, "name": "Activity 5", "details": "details iwiwiw838383" },
-    { "id": 6, "name": "Activity 6", "details": "details jshdhd64363636" },
-    { "id": 7, "name": "Activity 7", "details": "details kskskk 8383838" },
-    { "id": 8, "name": "Activity 8", "details": "details ksksksks 8282828" },
-    { "id": 9, "name": "Activity 9", "details": "details sksksksk " },
-    { "id": 10, "name": "Activity 10", "details": "details ksksksksksk" }
-];
+//var HEROES: Hero[] = [
+//    { "id": 1, "name": "Translation","details":"details ksksksks"},
+//    { "id": 2, "name": "Consultation","details":"details jsjsjsjs" },
+//    { "id": 3, "name": "Company Creation","details":"details 18182828" },
+//    { "id": 4, "name": "Activity 4","details":"details hshs828282" },
+//    { "id": 5, "name": "Activity 5","details":"details iwiwiw838383" },
+//    { "id": 6, "name": "Activity 6","details":"details jshdhd64363636" },
+//    { "id": 7, "name": "Activity 7","details":"details kskskk 8383838" },
+//    { "id": 8, "name": "Activity 8","details":"details ksksksks 8282828" },
+//    { "id": 9, "name": "Activity 9","details":"details sksksksk " },
+//    { "id": 10, "name": "Activity 10","details":"details ksksksksksk" }
+//];
 //# sourceMappingURL=app.js.map
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import {Component, bootstrap, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
-//import {Zippy} from 'app/zippy';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 class Hero {
     id: number;
@@ -9,17 +9,27 @@ class Hero {
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'templates/main.html'
-//    styleUrls:['style.css'],
-    
+    templateUrl: 'templates/main.html',
+    viewProviders: [HTTP_PROVIDERS],  
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 class AppComponent {
     public title = 'Volkov Alexander';
 	public selectedHero: Hero;
-    public heroes = HEROES;
+//    public heroes = HEROES;
+    
+    constructor(http: Http) {
+    http.get('activity.json')
+      // Call map on the response observable to get the parsed people object
+      .map(res => res.json()).subscribe(heroes => this.heroes = heroes);
+      // Subscribe to the observable to get the parsed people object and attach it to the
+      // component
+//      .subscribe(people => this.people = people);
+  }
+       
     onSelect(hero){
         this.selectedHero = hero;
+//         console.log(JSON.stringify(HEROES) );
    
     }
     getSelectedClass(hero: Hero) {
@@ -30,17 +40,17 @@ class AppComponent {
 
 bootstrap(AppComponent);
 
-var HEROES: Hero[] = [
-    { "id": 1, "name": "Translation","details":"details ksksksks"},
-    { "id": 2, "name": "Consultation","details":"details jsjsjsjs" },
-    { "id": 3, "name": "Company Creation","details":"details 18182828" },
-    { "id": 4, "name": "Activity 4","details":"details hshs828282" },
-    { "id": 5, "name": "Activity 5","details":"details iwiwiw838383" },
-    { "id": 6, "name": "Activity 6","details":"details jshdhd64363636" },
-    { "id": 7, "name": "Activity 7","details":"details kskskk 8383838" },
-    { "id": 8, "name": "Activity 8","details":"details ksksksks 8282828" },
-    { "id": 9, "name": "Activity 9","details":"details sksksksk " },
-    { "id": 10, "name": "Activity 10","details":"details ksksksksksk" }
-];
+//var HEROES: Hero[] = [
+//    { "id": 1, "name": "Translation","details":"details ksksksks"},
+//    { "id": 2, "name": "Consultation","details":"details jsjsjsjs" },
+//    { "id": 3, "name": "Company Creation","details":"details 18182828" },
+//    { "id": 4, "name": "Activity 4","details":"details hshs828282" },
+//    { "id": 5, "name": "Activity 5","details":"details iwiwiw838383" },
+//    { "id": 6, "name": "Activity 6","details":"details jshdhd64363636" },
+//    { "id": 7, "name": "Activity 7","details":"details kskskk 8383838" },
+//    { "id": 8, "name": "Activity 8","details":"details ksksksks 8282828" },
+//    { "id": 9, "name": "Activity 9","details":"details sksksksk " },
+//    { "id": 10, "name": "Activity 10","details":"details ksksksksksk" }
+//];
 
 
